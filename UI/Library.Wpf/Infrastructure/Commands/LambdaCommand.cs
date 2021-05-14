bd4ba112595f57ac3662dd912cb91c586a5483b4
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-14 09:20:01 +0500

    Базовая команда.

---
+++ UI/Library.Wpf/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using Library.Wpf.Infrastructure.Commands.Base;
+using System;
+
+namespace DOfficeCore.Infrastructure.Commands
+{
+    internal class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => execute(parameter);
+    }
+}
