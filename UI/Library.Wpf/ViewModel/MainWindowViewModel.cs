2724139e10c2272c271aba5d86543bf21aca8ef6
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-14 09:24:24 +0500

    Базовая модель-представление и модель представление главного окна

---
+++ UI/Library.Wpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DOfficeCore.ViewModels.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Wpf.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+
+        #region Свойства
+
+        #region Заголовок окна
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Библиотекарь";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
