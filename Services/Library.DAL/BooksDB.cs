afff45f79c6528fc6f71d354439890ad781b5441
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-15 15:40:34 +0500

    Заготовка инициализатора БД

--- Services/Library.DAL/BooksDB.cs
+++ Services/Library.DAL/BooksDB.cs
@@ -1,8 +1,5 @@
 ﻿using Library.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Library.DAL
 {
---
+++ Services/Library.DAL/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.DAL
+{
+    public class DBInitializer
+    {
+        public async Task Initialize()
+        {
+            var config = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BooksDB;Integrated Security=True";
+
+            using(var db = new BooksDB(new DbContextOptionsBuilder<BooksDB>().UseSqlServer(config).Options))
+            {
+                var created = await db.Database.EnsureCreatedAsync();
+            }
+        }
+    }
+}
