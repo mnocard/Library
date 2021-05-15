c6e1239de6dcc8681c38c127e9f4d862020b2bb5
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-15 10:25:10 +0500

    Рефакторинг хост сервиса

---
+++ Services/Library.WcfService.Host/HostService/TestHost.cs
@@ -0,0 +1,40 @@
+﻿using Library.WcfService.Services;
+using System.ServiceModel.Description;
+using System.ServiceModel;
+using System;
+using Library.WcfService.Interfaces;
+
+namespace Library.WcfService.Host.HostService
+{
+	public class TestHost
+	{
+		private const string _HostUri = "http://localhost:8733/Design_Time_Address/Library.WcfService/";
+		private const string _ServiceAddress = "Calculator";
+
+		public void Initialize()
+		{
+			var baseAddress = new Uri(_HostUri);
+			var selfHost = new ServiceHost(typeof(Calculator), baseAddress);
+
+			try
+			{
+				selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), _ServiceAddress);
+				var smb = new ServiceMetadataBehavior();
+				smb.HttpGetEnabled = true;
+				selfHost.Description.Behaviors.Add(smb);
+
+				selfHost.Open();
+				Console.WriteLine("The service is ready.\nPress <Enter> to terminate the service.");
+				Console.ReadLine();
+				selfHost.Close();
+			}
+			catch (CommunicationException ce)
+			{
+				Console.WriteLine("An exception occurred: {0}", ce.Message);
+				Console.ReadLine();
+				selfHost.Abort();
+			}
+		}
+
+	}
+}
--- Services/Library.WcfService.Host/Program.cs
+++ Services/Library.WcfService.Host/Program.cs
@@ -1,47 +1,22 @@
-﻿using Library.WcfService.Interfaces;
+﻿using Library.WcfService.Host.HostService;
+using Library.WcfService.Interfaces;
 using Library.WcfService.Services;
 using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Library.WcfService.Host
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // Step 1: Create a URI to serve as the base address.
-            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Address/Library.WcfService/");
+            var th = new TestHost();
+            await Task.Run(() => th.Initialize());
 
-            // Step 2: Create a ServiceHost instance.
-            ServiceHost selfHost = new ServiceHost(typeof(Calculator), baseAddress);
 
-            try
-            {
-                // Step 3: Add a service endpoint.
-                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "Calculator");
-
-                // Step 4: Enable metadata exchange.
-                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-                smb.HttpGetEnabled = true;
-                selfHost.Description.Behaviors.Add(smb);
-
-                // Step 5: Start the service.
-                selfHost.Open();
-                Console.WriteLine("The service is ready.");
-
-                // Close the ServiceHost to stop the service.
-                Console.WriteLine("Press <Enter> to terminate the service.");
-                Console.WriteLine();
-                Console.ReadLine();
-                selfHost.Close();
-            }
-            catch (CommunicationException ce)
-            {
-                Console.WriteLine("An exception occurred: {0}", ce.Message);
-                Console.ReadLine();
-                selfHost.Abort();
-            }
         }
     }
 }
