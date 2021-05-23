d7e0fe5220938080dfe161f5c9ba7c2ebce71e28
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-23 13:09:57 +0500

    Подключение BooksServiceClient к WPF приложению

--- UI/Library.Wpf/Infrastructure/Services/ServiceManager.cs
+++ UI/Library.Wpf/Infrastructure/Services/ServiceManager.cs
@@ -1,18 +1,26 @@
 ﻿using Library.Wpf.Infrastructure.Interfaces;
 using Library.Wpf.ServiceReference1;
+using Library.Wpf.ServiceReference2;
 
 namespace Library.Wpf.Infrastructure.Services
 {
     class ServiceManager : IServiceManager
     {
         private CalculatorClient _CalculatorClient;
+        private BooksServiceClient _BooksServiceClient;
         public ServiceManager()
         {
             _CalculatorClient = new CalculatorClient();
+            _BooksServiceClient = new BooksServiceClient();
         }
         public CalculatorClient GetTestService()
         {
             return _CalculatorClient;
+        }
+
+        public BooksServiceClient GetBooksService()
+        {
+            return _BooksServiceClient;
         }
     }
 }
